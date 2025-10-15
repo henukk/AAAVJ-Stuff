a991a3c070e480f2bdbe874eb43cc40a80b55058
henukk <hectornunezcarpio@gmail.com>
2025-10-15 22:07:32 +0200

    String hash optimization

--- AdvProgrammingExercises/AdvProgrammingExercises/String.h
+++ AdvProgrammingExercises/AdvProgrammingExercises/String.h
@@ -1,113 +1,39 @@
 #pragma once
 #include <iostream>
-#include <cassert>
 
+class StringHash;
 
-constexpr int UNDEFINED_HASH = -1;
+constexpr bool OPTIMIZED_COMPARATOR = false;
 class String {
 private:
 	char* chars;
-	int size;
-	int hash;
+	unsigned long long size;
+	StringHash* hash;
 
-	constexpr inline String(char* init, int size, int hash) noexcept : chars(init), size(size), hash(hash) {}
+	String(char* init, const unsigned long long size) noexcept;
 public:
 #pragma region CONSTRUCTORS & DESTRUCTORS
-	constexpr inline String() noexcept : chars(nullptr), size(0), hash(UNDEFINED_HASH) {}
-	inline String(const char* init) : size(0), hash(UNDEFINED_HASH) {
-		assert(init != nullptr && "ASSERT FAIL: String constructor: init pointer is null\n");
-		while (init[size] != '\0') {
-			++size;
-		}
+	String() noexcept;
+	String(const char* init);
+	String(const String& other);
+	String(String&& other) noexcept;
 
-		chars = new char[size + 1];
-		for (int i = 0; i < size; ++i) {
-			chars[i] = init[i];
-		}
-		chars[size] = '\0';
-	}
-	inline String(const String& other) : chars(nullptr), size(other.size), hash(other.hash) {
-		if (size == 0) {
-			chars = nullptr;
-		}
-		else {
-			chars = new char[size + 1];
-			for (int i = 0; i < size; ++i) {
-				chars[i] = other[i];
-			}
-			chars[size] = '\0';
-		}
-	}
-	inline String(String&& other) noexcept : chars(other.chars), size(other.size), hash(other.hash) {
-		other.chars = nullptr;
-		other.size = 0;
-		other.hash = UNDEFINED_HASH;
-	}
-
-	inline ~String() noexcept {
-		delete[] chars;
-	}
+	~String() noexcept;
 #pragma endregion
 
 #pragma region GETTERS
-	constexpr inline int getSize() const noexcept {
-		return size;
-	}
-	constexpr inline int getHash() const noexcept {
-		if (hash == UNDEFINED_HASH) {
-			;// hash = calculateHash();
-		}
-		return hash;
-	}
-	constexpr inline bool isEmpty() const noexcept {
-		return size == 0;
-	}
+	unsigned long long getSize() const noexcept;
+	bool isEmpty() const noexcept;
 #pragma endregion
 
 #pragma region SETTERS
 #pragma endregion
 
 #pragma region OPERATORS
-	inline char& operator[](int i) { //modificador de carácter -> por eso reset hash
-		assert(chars != nullptr && "ASSERT FAIL: String::operator[] - chars is null");
-		assert(i >= 0 && i < size && "ASSERT FAIL: String::operator[] - index out of bounds");
-		hash = UNDEFINED_HASH;
-		return chars[i];
-	}
-	constexpr char operator[](int i) const noexcept { //consultor de carácter
-		assert(chars != nullptr && "ASSERT FAIL: String::operator[] - chars is null");
-		assert(i >= 0 && i < size && "ASSERT FAIL: String::operator[] - index out of bounds");
-		return chars[i];
-	}
-	constexpr bool operator==(const String& other) const noexcept {
-		assert(chars != nullptr && "String::operator== - left operand is null");			//en RELEASE no se que hará
-		assert(other.chars != nullptr && "String::operator== - right operand is null");		//en RELEASE no se que hará
-		if (!chars || !other.chars) return chars == other.chars;
-		if (size != other.size) return false;
-		if (getHash() != other.getHash()) return false;
-
-		bool result = true;
-		int curr = 0;
-		while (result && curr < size) { //creo que prevé mejor los saltos la cpu así
-			result = chars[curr] == other[curr];
-			++curr;
-		}
-		return result;
-	}
-	inline String operator+(const String& other) const {
-		//assert(chars != nullptr && "String::operator+ - left operand is null");
-		//assert(other.chars != nullptr && "String::operator+ - right operand is null");
-		int newSize = size + other.size;
-		char* newChars = new char[newSize + 1];
-
-		for (int i = 0; i < size; ++i)
-			newChars[i] = chars[i];
-		for (int i = 0; i < other.size; ++i)
-			newChars[i + size] = other[i];
-		newChars[newSize] = '\0';
-
-		return String(newChars, newSize, UNDEFINED_HASH);
-	}
+	char& operator[](unsigned long long i);
+	char operator[](unsigned long long i) const noexcept;
+	bool operator==(const String& other) const noexcept;
+	String operator+(const String& other) const;
 	friend std::ostream& operator<<(std::ostream& os, const String& s) noexcept {
 		if (!s.chars) return os;
 		os << s.chars;
@@ -116,27 +42,12 @@ public:
 #pragma endregion
 
 #pragma region FUNCTIONS
-	constexpr inline int length() const noexcept {
-		return size;
-	}
-	inline void clear() noexcept {
-		assert(chars != nullptr && "String::clear() - already null or not initialized");
-		delete[] chars;
-		chars = nullptr;
-		size = 0;
-		hash = UNDEFINED_HASH;
-	}
+	unsigned long long length() const noexcept;
+	void clear() noexcept;
 #pragma endregion
 
 #pragma region UTILS/HELPERS
-	static inline String GetMeAString() { return String("another string"); }
-	inline void print() const noexcept {
-		if (chars == nullptr) {
-			std::cout << "(null)\n";
-		}
-		else {
-			std::cout << *this << '\n';
-		}
-	}
+	static String GetMeAString() { return String("another string"); }
+	void print() const noexcept;
 #pragma endregion	
 };
---
+++ AdvProgrammingExercises/AdvProgrammingExercises/StringHash.cpp
@@ -0,0 +1,32 @@
+#include "StringHash.h"
+
+StringHash::StringHash() noexcept : hash(0), hashed(false) {}
+StringHash::StringHash(const StringHash& other) noexcept : hash(other.hash), hashed(other.hashed) {}
+StringHash::~StringHash() noexcept {}
+
+bool StringHash::operator==(const StringHash& other) const noexcept {
+	return (hash == other.hash);
+}
+
+bool StringHash::getHashed() const noexcept {
+	return hashed;
+}
+
+unsigned long long StringHash::getHash() const noexcept {
+	return hash;
+}
+
+void StringHash::generateHash(const unsigned long long size, const char* chars) noexcept {
+	hash = 0;
+	unsigned long long p_pow = 1;
+	for (unsigned long long i = 0; i < size; ++i) {
+		hash = (hash + (chars[i] - 'a' + 1) * p_pow) % MAX_HASH;
+		p_pow = (p_pow * p) % MAX_HASH;
+	}
+	hashed = true;
+}
+
+void StringHash::reset() noexcept {
+	hash = 0;
+	hashed = false;
+}
